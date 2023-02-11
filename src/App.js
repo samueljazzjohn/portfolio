4f20c0f729a29ace83dc0b7c4a969a9ecaa4a32d
samueljazzjohn <samueljazzjohn@gmail.com>
2023-02-11 23:43:55 +0530

    some resources added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          Learn React
-      </header>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 

f954c14e4d3c90b5e5ffebfc05f869fc45f6155e
samueljazzjohn <samueljazzjohn@gmail.com>
2023-03-15 22:10:13 +0530

    initial setup completed

---
+++ portfolio/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <Head>
+          <title className='text-red-500'> Jazz Portfolio </title>
+        </Head>
+      </div>
+    </>
+  )
+}

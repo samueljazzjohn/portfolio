f5a85caeff5494c18fbab22c0a343acaacb6932b
samueljazzjohn <samueljazzjohn@gmail.com>
2023-04-15 17:17:05 +0530

    fix:responsive element added

--- portfolio/components/About.tsx
+++ portfolio/components/About.tsx
@@ -19,10 +19,10 @@ export default function About({pageinfo }: Props) {
         transition={{
             duration:1.5
         }}
-        className='relative flex flex-col h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+        className='relative flex flex-col h-screen text-center md:text-left md:flex-row max-w-7xl px-3 md:px-10 justify-evenly mx-auto items-center'>
             <h3 className='absolute top-24 uppercase text-gray-500 tracking-[20px] text-2xl'>About</h3>
-            <motion.img
-                className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover
+            <motion.img 
+                className='-mb-[100px] md:mb-0 flex-shrink-0 w-44 h-44 rounded-full object-cover
         md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]'
                 initial={{
                     x: -200,
@@ -39,7 +39,7 @@ export default function About({pageinfo }: Props) {
                 }}
                 src={urlFor(pageinfo?.profilePic).url()}
             />
-        <div className='space-y-10 px-0 md:px-10'>
+        <div className='-mb-[100px] md:mb-0 space-y-10 px-0 md:px-10'>
             <h4 className='text-4xl font-semibold'>Here is a <span className='underline decoration-[#F7AB0A]/50'>little</span> background</h4>
             <p className='text-sm md:text-base'>{pageinfo.backgroundInformation}</p>
         </div>

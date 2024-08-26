6cd7b6da6c6ac08cc83fb631366fdd1bbca39292
jazzjohon <samueljazzjohn@gmail.com>
2024-08-27 00:27:51 +0530

    removed spaces

--- portfolio/utils/fetchPageInfo.ts
+++ portfolio/utils/fetchPageInfo.ts
@@ -4,7 +4,5 @@ export const fetchPageinfo = async()=>{
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`)
     const data = await res.json()
     const pageinfo : Pageinfo = data.pageinfo;
-
-
     return pageinfo
 }
